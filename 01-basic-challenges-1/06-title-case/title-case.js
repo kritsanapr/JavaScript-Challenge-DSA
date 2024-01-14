7920930c7eb4d489a3771ac5385007a738c60e2f
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-14 22:26:26 +0700

    solve: title case problem

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,12 @@
-function titleCase() {}
+function titleCase(str) {
+    const wordArray = str.toLowerCase().split(' ');
+
+    for (let i = 0; i < wordArray.length; i++) {
+        wordArray[i] = wordArray[i][0].toUpperCase() + wordArray[i].slice(1);
+    }
+
+    console.log(wordArray.join(' '))
+    return wordArray.join(' ');
+}
 
 module.exports = titleCase;
