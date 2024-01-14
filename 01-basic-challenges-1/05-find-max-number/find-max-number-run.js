d26a159cc1dd6d100d29dc31ce38cdae319488e8
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-14 21:32:19 +0700

    solve: find max number in array  problem

--- 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number-run.js
@@ -1,5 +1,5 @@
 const findMaxNumber = require('./find-max-number');
 
-const result = findMaxNumber([2, 1, 9, 16, 10]);
+const result = findMaxNumber([2, 1, 9, 16, 10, 20]);
 
 console.log(result);
--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,17 @@
-function findMaxNumber() {}
+// function findMaxNumber(arr) {
+//     let max = arr[0];
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (max < arr[i]) {
+//             max = arr[i]
+//         }
+//     }
+
+//     return max;
+// }
+
+function findMaxNumber(arr) {
+    return Math.max(...arr);
+}
 
 module.exports = findMaxNumber;
