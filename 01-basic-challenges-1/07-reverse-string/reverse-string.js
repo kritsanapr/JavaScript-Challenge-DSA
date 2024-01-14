0b3e719b8654a7e0f74bbb1181eb7705f51cea39
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-14 22:40:57 +0700

    solv: reverse string problem

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,21 @@
-function reverseString() {}
+// function reverseString(str) {
+//     const strArray = str.split('');
+//     const reversedArray = strArray.reverse();
+//     const reversedString = reversedArray.join('');
+
+
+//     return reversedString;
+// }
+
+function reverseString(str) {
+    let reversedString = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str[i];
+    }
+
+
+    return reversedString;
+}
 
 module.exports = reverseString;
