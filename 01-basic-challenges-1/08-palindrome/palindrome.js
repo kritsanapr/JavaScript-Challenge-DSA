0084a75d0a984d68002daf4b8aaf9ee59df76b94
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-14 22:49:04 +0700

    solv: palindrome

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,18 @@
-function isPalindrome() {}
+// function isPalindrome(str) {
+// str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//     return str === str.split('').reverse().join('');
+// }
+
+function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    console.log(str);
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 module.exports = isPalindrome;
