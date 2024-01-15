e22bb128d3777ac16a81ac4a4420d038871851aa
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-15 20:02:48 +0700

    solv: count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,15 @@
-function countVowels() {}
+function countVowels(word) {
+    const transformedWord = word.toLowerCase();
+    let count = 0;
+
+    for (let i = 0; i < transformedWord.length; i++) {
+        const char = transformedWord[i];
+        if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 module.exports = countVowels;
