fb951463c0759a293fe3dd48bf954779ee1ed2d9
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-15 20:06:37 +0700

    remove duplicate

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,15 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+    const uniqueValue = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueValue.includes(arr[i])) {
+            uniqueValue.push(arr[i])
+        }
+    }
+
+    return uniqueValue
+}
+
+
 
 module.exports = removeDuplicates;
