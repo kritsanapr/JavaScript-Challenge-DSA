1fb4684296ae317f3f25558fef349ace641f2d87
kritsanapr <kritsana.pr.41@gmail.com>
2024-01-15 20:09:17 +0700

    try other solution

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,15 +1,21 @@
+// Solution 1
+// function removeDuplicates(arr) {
+//     const uniqueValue = []
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (!uniqueValue.includes(arr[i])) {
+//             uniqueValue.push(arr[i])
+//         }
+//     }
+
+//     return uniqueValue
+// }
+
+// Solution 2
 function removeDuplicates(arr) {
-    const uniqueValue = []
-
-    for (let i = 0; i < arr.length; i++) {
-        if (!uniqueValue.includes(arr[i])) {
-            uniqueValue.push(arr[i])
-        }
-    }
-
-    return uniqueValue
+    const uniqueValue = new Set(arr)
+    // return [...uniqueValue]
+    return Array.from(uniqueValue)
 }
-
-
 
 module.exports = removeDuplicates;
